bd9821f3bae2b8d98951c6d9d19bc3e355282fdc
Ubuntu <ubuntu@ip-172-31-38-34.us-west-2.compute.internal>
2026-04-12 16:47:30 +0000

    update main tf file

--- day-01/terraform-basics/main.tf
+++ day-01/terraform-basics/main.tf
@@ -14,3 +14,12 @@ provider "aws" {
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "shibnath-terraform-day61-unique"
 }
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0d76b909de1a0595d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "TerraWeek-Modified"
+  }
+}
