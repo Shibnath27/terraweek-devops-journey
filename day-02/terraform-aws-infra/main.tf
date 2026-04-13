9e771572dcb7d9c3097af6d1d1f01a44d6a3d419
Ubuntu <ubuntu@ip-172-31-38-34.us-west-2.compute.internal>
2026-04-13 11:38:39 +0000

    Day 02: Added VPC infrastructure, dependency graph, and lifecycle configuration

--- day-02/terraform-aws-infra/main.tf
+++ day-02/terraform-aws-infra/main.tf
@@ -78,6 +78,9 @@ resource "aws_instance" "main" {
   vpc_security_group_ids      = [aws_security_group.sg.id]
   associate_public_ip_address = true
 
+  lifecycle {
+   create_before_destroy = true
+  }
   tags = {
     Name = "TerraWeek-Server"
   }
